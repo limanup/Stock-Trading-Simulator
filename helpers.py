3ef9403da1624eb772c4e3e864731acf7bb8f37f
Liman <liman123shen@gmail.com>
2022-07-12 17:14:34 -0400

    fix api_key

--- helpers.py
+++ helpers.py
@@ -38,31 +38,32 @@ def login_required(f):
 def lookup(symbol):
     """Look up quote for symbol."""
 
-    return {
-        "name": symbol,
-        "price": 100,
-        "symbol": symbol
-    }
+    # # for test purpose:
+    # return {
+    #     "name": symbol,
+    #     "price": 100,
+    #     "symbol": symbol
+    # }
 
-    # # Contact API
-    # try:
-    #     api_key = os.environ.get("API_KEY")
-    #     url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
-    #     response = requests.get(url)
-    #     response.raise_for_status()
-    # except requests.RequestException:
-    #     return None
+    # Contact API
+    try:
+        api_key = os.environ.get("API_KEY")
+        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
 
-    # # Parse response
-    # try:
-    #     quote = response.json()
-    #     return {
-    #         "name": quote["companyName"],
-    #         "price": float(quote["latestPrice"]),
-    #         "symbol": quote["symbol"]
-    #     }
-    # except (KeyError, TypeError, ValueError):
-    #     return None
+    # Parse response
+    try:
+        quote = response.json()
+        return {
+            "name": quote["companyName"],
+            "price": float(quote["latestPrice"]),
+            "symbol": quote["symbol"]
+        }
+    except (KeyError, TypeError, ValueError):
+        return None
 
 
 def usd(value):
